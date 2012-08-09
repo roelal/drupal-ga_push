b31aa696d5faf25b2f28f447348adc194d82a455
tunic <ricardo@metadrop.net>
2012-08-09 21:03:03 +0200

    Removed debug JS code.

--- modules/browser/ga_push_browser.js
+++ modules/browser/ga_push_browser.js
@@ -12,9 +12,6 @@
         $(value['selector']).bind(value['bind'], function() {
 
           if (typeof(_gaq) == 'object') {
-            if (typeof(console) == 'object' && typeof(console.log) == 'function') {
-              console.log('push (' + value['push'][0] + ' | ' + value['push'][1] + ' | ' + value['push'][2] + ' | ' + value['push'][3] + ')');
-            }
             _gaq.push(['_trackEvent', value['push'][0], value['push'][1], value['push'][2] , value['push'][3]]);
           }
 
