5451583345402c1269e8f3586d8ad747ae05f9e9
tunic <ricardo@metadrop.net>
2012-08-09 21:04:10 +0200

    Removed JS behaviors code.
    Code must be executed just once and as no behaviors are attached to content
    Drupal.behaviors is not needed.

--- modules/browser/ga_push_browser.js
+++ modules/browser/ga_push_browser.js
@@ -1,26 +1,11 @@
 (function ($) {
+  ga_push_browser = Drupal.settings.ga_push_browser;
 
-  Drupal.behaviors.ga_push_browser = {
-    attach: function (context, settings) {
-
-      ga_push_browser = Drupal.settings.ga_push_browser;
-
-      /* TODO: preparar para que solo salte una vez */
-
-      $.each(ga_push_browser, function(index, value) {
-
-        $(value['selector']).bind(value['bind'], function() {
-
-          if (typeof(_gaq) == 'object') {
-            _gaq.push(['_trackEvent', value['push'][0], value['push'][1], value['push'][2] , value['push'][3]]);
-          }
-
-        });
-
-      });
-
-
-    } //Behaviors
-  }
-
+  $.each(ga_push_browser, function(index, value) {
+    $(value['selector']).bind(value['bind'], function() {
+      if (typeof(_gaq) == 'object') {
+        _gaq.push(['_trackEvent', value['push'][0], value['push'][1], value['push'][2] , value['push'][3]]);
+      }
+    });
+  });
 })(jQuery);
