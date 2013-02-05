0a42267af62d70d04b8d2725a9c754ac4fb274aa
tunic <ricardo@metadrop.net>
2013-02-05 19:08:55 +0100

    Bugfix: Using Drupal Behaviors for attaching GA Push browser events.

--- modules/browser/ga_push_browser.js
+++ modules/browser/ga_push_browser.js
@@ -1,11 +1,16 @@
 (function ($) {
-  ga_push_browser = Drupal.settings.ga_push_browser;
-
-  $.each(ga_push_browser, function(index, value) {
-    $(value['selector']).bind(value['bind'], function() {
-      if (typeof(_gaq) == 'object') {
-        _gaq.push(['_trackEvent', value['push'][0], value['push'][1], value['push'][2] , value['push'][3]]);
-      }
-    });
-  });
+  Drupal.behaviors.ga_push_browser = {
+    attach: function (context, settings) {
+      jQuery.each(Drupal.settings.ga_push_browser, function(index, value) {
+        $(value['selector'], context).once('ga_push_browser_listener', function () {
+          $elem = $(this);
+          $elem.bind(value['bind'], function() {
+            if (typeof(_gaq) == 'object') {
+              _gaq.push(['_trackEvent', value['push'][0], value['push'][1], value['push'][2] , value['push'][3]]);
+            }
+          });
+        });
+      });
+    }
+  };
 })(jQuery);
